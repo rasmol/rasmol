e6b965307fa1161cf943e78e4e2d0c5bc48bdedd
Herbert J. Bernstein <yayahjb@gmail.com>
2006-09-16 12:31:03 +0000

    Cleanup of symlinks and extra files -- HJB

--- src/build_MACOSX.sh
+++ src/build_MACOSX.sh
@@ -8,6 +8,7 @@
 cp Imakefile Imakefile_save
 rm Imakefile
 echo "#define PIXELDEPTH 8"|cat - Imakefile_save > Imakefile
+echo "#define MACOSX_FINK xx">> Imakefile
 xmkmf
 mv Makefile Makefile_8BIT
 if [ "$1"  != "-nobuild" ]; then
@@ -17,6 +18,7 @@ if [ "$1"  != "-nobuild" ]; then
 fi;
 rm Imakefile
 echo "#define PIXELDEPTH 16"|cat - Imakefile_save > Imakefile
+echo "#define MACOSX_FINK xx">> Imakefile
 xmkmf
 mv Makefile Makefile_16BIT
 if [ "$1" != "-nobuild" ]; then
@@ -26,17 +28,27 @@ if [ "$1" != "-nobuild" ]; then
 fi;
 rm Imakefile
 echo "#define PIXELDEPTH 32"|cat - Imakefile_save > Imakefile
+echo "#define MACOSX_FINK xx">> Imakefile
+gcc checklong.c -o checklong
+VAL=`(./checklong; echo $1)`
+if [ $VAL -gt 4 ]; then
+  mv rasmol.h rasmol_amd64_save.h
+echo "#define _LONGLONG"|cat - rasmol_amd64_save.h > rasmol.h
+fi;
 xmkmf
 mv Makefile Makefile_32BIT
 if [ "$1" != "-nobuild" ]; then
   make -f Makefile_32BIT clean
   make -f Makefile_32BIT rasmol
-  mv rasmol rasmol_32BIT
+  mv rasmol rasmol_32BIT  
+  if [ $VAL -gt 4 ]; then  
+    mv rasmol_amd64_save.h rasmol.h
+  fi;
 fi;
 rm Imakefile
 echo "#define PIXELDEPTH 8" > Imakefile
-echo "#define MACOSX_FINK xx">> Imakefile
 echo "#define XFORMSLIB xx"|cat - Imakefile_save >> Imakefile
+echo "#define MACOSX_FINK xx">> Imakefile
 xmkmf
 mv Makefile Makefile_8BIT
 if [ "$1"  != "-nobuild" ]; then
@@ -59,11 +71,19 @@ rm Imakefile
 echo "#define PIXELDEPTH 32" > Imakefile
 echo "#define MACOSX_FINK xx">> Imakefile
 echo "#define XFORMSLIB xx"|cat - Imakefile_save >> Imakefile
+if [ $VAL -gt 4 ]; then
+  mv rasmol.h rasmol_amd64_save.h
+echo "#define _LONGLONG"|cat - rasmol_amd64_save.h > rasmol.h
+fi;
 xmkmf
 mv Makefile Makefile_32BIT
 if [ "$1" != "-nobuild" ]; then
   make -f Makefile_32BIT clean
   make -f Makefile_32BIT rasmol
   mv rasmol rasmol_XFORMS_32BIT
+  if [ $VAL -gt 4 ]; then  
+    mv rasmol_amd64_save.h rasmol.h
+  fi;
 fi;
 mv Imakefile_save Imakefile
+rm checklong
